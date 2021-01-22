e92f62393bb4e07493b062b67219e07b9f171cb4
Elikson Bastos Tavares <Eliksonbt.eks@gmail.com>
2021-01-22 14:33:25 -0400

    [Add] Project files

---
+++ app/src/main/java/com/example/testewebview/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.testewebview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myWebView: WebView = findViewById(R.id.webview)
+        myWebView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                if (url != null) {
+                    view?.loadUrl(url)
+                }
+                return true
+            }
+        }
+        myWebView.loadUrl("http://192.168.0.11")
+        myWebView.settings.javaScriptEnabled = true
+    }
+}
